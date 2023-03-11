fe227b3829c2fd89c326b8e23951e128a79ba8c8
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-11 14:22:25 +0600

    update

--- local-storage/local.js
+++ local-storage/local.js
@@ -22,6 +22,30 @@ const submit = () =>{
         localStorage.setItem('UserDetails',userDetailsString);
 
         // getting the item as obj
+        const userDetailsget = localStorage.getItem('UserDetails');
+        const userDetailsgetString = JSON.parse(userDetailsget);
+
+        console.log(userDetailsgetString.email)
+        // const {email,password} = userDetailsString;
+
+        const sectionContainer = document.getElementById('section-container');
+
+        const div = document.createElement('div');
+        
+        div.innerHTML =`
+        <div class="text-center">
+        <div class="card-body">
+            <h5 class="card-title">${userDetailsgetString.email}</h5>
+            <p class="card-text">${userDetailsgetString.password}</p>
+        </div>
+        </div>
+        `
+        sectionContainer.appendChild(div);
+        
     }
     // console.log(emailValue,passwordValue);
+}
+
+const reset = () =>{
+    localStorage.clear();
 }
