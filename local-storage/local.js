949ab429b32a16bb7bca60839540a445d025c25f
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-11 13:53:41 +0600

    Update local.js

--- local-storage/local.js
+++ local-storage/local.js
@@ -1,3 +1,12 @@
 const submit = () =>{
-    console.log('first')
+    const email = document.getElementById('email');
+    const emailValue = email.value;
+
+    const password = document.getElementById('password');
+    const passwordValue = password.value;
+
+    if(emailValue=='' && passwordValue==''){
+        alert('Nothing entered here...!');
+    }
+    // console.log(emailValue,passwordValue);
 }
